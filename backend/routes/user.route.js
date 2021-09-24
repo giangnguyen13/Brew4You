c94af6c3945f3b5cb4ab3942acd523ba09a91aaf
Giang Nguyen <giangkawaii1999@gmail.com>
2021-09-24 18:25:30 -0400

    show logged user info successfully

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,17 @@
 import express from "express";
+import { protect } from "../middleware/auth.middleware.js";
 
-import { authUser, registerUser } from "../controllers/user.controller.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.route("/login").post(authUser);
 router.route("/").post(registerUser);
+
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
