84dc7570ddd827cc237b12c177f9391308261de2
Franndy Abreu <franndyabreu@gmail.com>
2021-10-03 12:54:11 -0400

    chore (cart): Added notifications

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -5,7 +5,6 @@ import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
 
 const ProfileScreen = () => {
   const [userProfile, setUserProfile] = useState({});
