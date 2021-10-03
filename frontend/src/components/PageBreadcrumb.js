49dbfa317752d766b52e9668888aaa22fd1a86df
Ha <hanghenguyen@gmail.com>
2021-10-03 14:36:30 -0400

    tracking screen updated

--- frontend/src/components/PageBreadcrumb.js
+++ frontend/src/components/PageBreadcrumb.js
@@ -1,24 +1,22 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const PageBreadcrumb = () => {
+  const history = useHistory();
+  const handleBackToHomePage = () => {
+    history.push("/menu/all")
+  }
+
   return (
     <section className='section-pagetop bg-light mb-5'>
       <div className='container'>
-        <h2 className='title-page'>Dynamic content</h2>
+        <h2 className='title-page'>Tracking Order</h2>
         <nav>
           <ol className='breadcrumb'>
             <li className='breadcrumb-item'>
-              <a href='https://bootstrap-ecommerce.com/bootstrap-ecommerce-html/page-listing-grid.html#'>
+              <a onClick={handleBackToHomePage} style={{color: "#89624c", cursor: "pointer", fontWeight: 500}}>
                 Home
               </a>
-            </li>
-            <li className='breadcrumb-item'>
-              <a href='https://bootstrap-ecommerce.com/bootstrap-ecommerce-html/page-listing-grid.html#'>
-                Best category
-              </a>
-            </li>
-            <li className='breadcrumb-item active' aria-current='page'>
-              Dynamic content
             </li>
           </ol>
         </nav>
--- frontend/src/screens/TrackingScreen.js
+++ frontend/src/screens/TrackingScreen.js
@@ -1,24 +1,113 @@
 import React, { useEffect, useState } from "react";
 import PageBreadcrumb from "../components/PageBreadcrumb";
-
+import { FaCheckCircle } from "react-icons/fa";
 import ProgressBar from "react-bootstrap/ProgressBar";
 
 const ProductListScreen = () => {
+  const [trackingOrder, setTrackingOrder] = useState("");
+  const [displayStatus, setDisplayStatus] = useState(false);
+
+  const handleTracking = () => {
+    //API call get status of tracking order
+
+    setDisplayStatus(true);
+  };
+
   return (
     <>
       <PageBreadcrumb />
-      <ProgressBar>
-        <ProgressBar variant='success' now={33} key={1} />
-        <ProgressBar variant='warning' now={33} key={2} />
-        <ProgressBar striped variant='info' now={30} key={3} />
-      </ProgressBar>
-      <br />
-
-      <div className='d-flex justify-content-between'>
-        <p className='h5'>Prepared your drink</p>
-        <p className='h5'>Out for develivery</p>
-        <p className='h5'>Arriving</p>
-      </div>
+      <form class='form-inline' action=''>
+        <div class='row justify-content-center'>
+          <div class='col-3'>
+            <input
+              type='text'
+              onChange={(event) => setTrackingOrder(event.target.value)}
+              id='order_id'
+              class='form-control'
+              placeholder='Enter tracking number here'
+            />
+          </div>
+          <div class='col-2'>
+            <a id='track_btn' class='btn btn-success' onClick={handleTracking}>
+              Track order
+            </a>
+          </div>
+        </div>
+      </form>
+      {displayStatus && (
+        <>
+          <h4 style={{color: "#89624c"}}>Status Updates:</h4>
+          <table class='table table-bordered'>
+            <thead>
+              <tr>
+                <th scope='col'>Action</th>
+                <th scope='col'>Status</th>
+                <th scope='col'>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Delivered</td>
+                <td>
+                  {" "}
+                  <FaCheckCircle color='#50C878' />
+                </td>
+                <td>
+                  {new Date(Date.now() - 30 * 60 * 1000).toLocaleString(
+                    "en-US"
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <td>Shipped</td>
+                <td>
+                  {" "}
+                  <FaCheckCircle color='#50C878' />
+                </td>
+                <td>
+                  {new Date(Date.now() - 20 * 60 * 1000).toLocaleString(
+                    "en-US"
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <td>Ready</td>
+                <td>
+                  {" "}
+                  <FaCheckCircle color='#50C878' />
+                </td>
+                <td>
+                  {new Date(Date.now() - 10 * 60 * 1000).toLocaleString(
+                    "en-US"
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <td>Ordered</td>
+                <td>Pending</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      )}
+      <h4 style={{color: "#89624c"}}>Order History</h4>
+      <table class='table' id='table02'>
+        <thead class='table-dark'>
+          <tr>
+            <th scope='col'>Items</th>
+            <th scope='col'>Quantity</th>
+            <th scope='col'>Unit Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Hot Chocolate, Latte</td>
+            <td>1</td>
+            <td>$34</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 };
