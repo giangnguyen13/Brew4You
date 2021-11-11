105d5ce4960f75b4017017e2a59bb1f3a88cf4dc
Giang Nguyen <giangkawaii1999@gmail.com>
2021-11-11 12:48:41 -0500

    Remove custom procfile

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,14 @@ import morgan from "morgan";
 import path from "path";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import {
-  notFound,
-  errorHandler
-} from "./middleware/error.middleware.js";
+import { notFound, errorHandler } from "./middleware/error.middleware.js";
 import productRoutes from "./routes/product.route.js";
-import StripeRoutes from './routes/stripe.route.js'
+import StripeRoutes from "./routes/stripe.route.js";
 import userRoutes from "./routes/user.route.js";
 import orderRoutes from "./routes/order.route.js";
-import StaffRoutes from './routes/staff.route.js'
+import StaffRoutes from "./routes/staff.route.js";
 
-
-import {
-  importData,
-  destroyData
-} from "./seeder.js";
-
+import { importData, destroyData } from "./seeder.js";
 
 // Load ENV variable to process.env variable
 dotenv.config();
@@ -34,15 +26,17 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(express.json());
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(productRoutes);
 app.use(orderRoutes);
 app.use("/api/user", userRoutes);
-app.use("/api/stripe", StripeRoutes)
-app.use("/api/admin", StaffRoutes)
+app.use("/api/stripe", StripeRoutes);
+app.use("/api/admin", StaffRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
@@ -59,9 +53,9 @@ app.use(notFound);
 
 app.use(errorHandler);
 
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} `)
+  console.log(`Server running in ${process.env.NODE_ENV} port ${PORT}`)
 );
