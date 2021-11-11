e8a798c1735eb473f2272b1a99693e8b314a318a
Giang Nguyen <giangkawaii1999@gmail.com>
2021-11-11 13:06:40 -0500

    fix proxy client app to fetch data from server api

--- frontend/src/services/api/config.js
+++ frontend/src/services/api/config.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.REACT_APP_ENV === "production"
+    ? "/api"
+    : "http://127.0.0.1:5000/api";
+
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: baseURL,
   timeout: 10000,
   headers: {
     "X-Requested-Width": "XMLHttpRequest",
